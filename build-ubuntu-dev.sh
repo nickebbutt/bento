7e096609d67ee363f3c685df8e2ac24bac5eb5e9
Nick Ebbutt <nick@ebbutt.net>
2019-08-05 12:39:09 +0100

    Install vbox and packer

--- build-ubuntu-dev.sh
+++ build-ubuntu-dev.sh
@@ -2,6 +2,13 @@
 
 # Create Nick's ubuntu dev box
 
+echo "Installing Packer"
+export VER="1.4.2"
+wget https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
+unzip packer_${VER}_linux_amd64.zip
+mv packer /usr/local/bin
+
+
 packer --version
 
 cd packer_templates/ubuntu
