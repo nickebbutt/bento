3e316c408380f845d3984c12c2e3c8e0746d2d57
Nick Ebbutt <nick@ebbutt.net>
2019-08-10 17:35:17 +0100

    Add openjdk

--- packer_templates/ubuntu/scripts/apt_install_tools.sh
+++ packer_templates/ubuntu/scripts/apt_install_tools.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -eux
 
+exitOnError() {
+    exit_code=$1
+    last_command=${@:2}
+    if [ $exit_code -ne 0 ]; then
+        >&2 echo "\"${last_command}\" command failed with exit code ${exit_code}."
+        exit $exit_code
+    fi
+}
+
+# enable !! command completion
+set -o history -o histexpand
+
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
   vim \
   curl \
@@ -10,6 +22,7 @@ apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
   openssh-client \
   openssl \
   git \
+  git-lfs \
   git-man \
   lsof \
   telnet \
@@ -26,10 +39,23 @@ apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
   dos2unix \
   docker.io \
   docker-compose 
-  
+exitOnError $? !!
   
 # Install ubuntu desktop and virtual box additions  
 apt-get install -y ubuntu-desktop 
+exitOnError $? !!
 sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
+exitOnError $? !!
 
-echo "Installed tools"
+# Install chrome
+CHROME_PKGNAME=google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/${CHROME_PKGNAME}
+exitOnError $? !!
+dpkg -i ${CHROME_PKGNAME}
+exitOnError $? !!
+rm ${CHROME_PKGNAME}
+
+apt-get install -y openjdk-11-jdk
+exitOnError $? !!
+
+echo "Finished installing tools"
